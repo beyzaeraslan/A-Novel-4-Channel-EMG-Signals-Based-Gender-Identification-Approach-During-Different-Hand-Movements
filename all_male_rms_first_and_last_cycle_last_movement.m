e420ba460536c6af9708021275f9b70e685ebc8e
beyzaeraslan <72105175+beyzaeraslan@users.noreply.github.com>
2025-05-20 22:16:57 +0300

    Update all_male_rms_first_and_last_cycle_last_movement.m

--- all_male_rms_first_and_last_cycle_last_movement.m
+++ all_male_rms_first_and_last_cycle_last_movement.m
@@ -1,20 +1,22 @@
 
 clear;
 clc;
-load('all_male_rms')
+load('all_male_rms')  % Load preprocessed RMS feature matrix for all male subjects
 
+ % Extract the last movement for each of the all male subjects
  for i = 1:100 
     base_idx = (i - 1) * 600; 
     tenth_move{i}   = all_male_rms(base_idx +540 +   1 : base_idx + 600, :);
  end
 
-
+% Select the first cycle's last movement for 20 male subjects 
 for i = 1:5:100
     varName = sprintf('all_male_rms_first_cycle_last_movement_%d', i);
     assignin('base', varName, tenth_move{i});
 end
 
 
+% Concatenate the selected first-cycle last movements into a single matrix
 male_concat_data = [];
 
 for i = 1:5:100
@@ -25,17 +27,18 @@ for i = 1:5:100
     end
 end
 
+% Save the final combined matrix for first-cycle last movements
 all_male_rms_first_cycle_last_movement = male_concat_data;
 save all_male_rms_first_cycle_last_movement all_male_rms_first_cycle_last_movement 
 
 
-
+% Select the last cycle's last movement for 20 male subjects
 for i = 5:5:100
     varName = sprintf('all_male_rms_last_cycle_last_movement_%d', i);
     assignin('base', varName, tenth_move{i});
 end
 
-
+% Concatenate the selected last-cycle last movements into a single matrix
 male_concat_data_2 = [];
 
 for i = 5:5:100
@@ -45,6 +48,7 @@ for i = 5:5:100
         male_concat_data_2 = [male_concat_data_2; data]; 
     end
 end
- 
+
+ % Save the final combined matrix for last-cycle last movements
 all_male_rms_last_cycle_last_movement = male_concat_data_2;
 save all_male_rms_last_cycle_last_movement all_male_rms_last_cycle_last_movement 
